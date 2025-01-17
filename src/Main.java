0428f507fbc0260095a8bb13b6ba787ab2bac19d
potados99 <potados99@gmail.com>
2025-01-17 12:28:21 +0900

    chore: 가장 기초적인 소켓 리스닝 코드!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.net.*;
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        try {
+            ServerSocket serverSocket = new ServerSocket();
+
+            serverSocket.bind(new InetSocketAddress("0.0.0.0", 8080));
+
+            Socket socket = serverSocket.accept();
+
+            DataInputStream inputStream = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+
+            try {
+                while (true) {
+                    byte read = inputStream.readByte();
+                    System.out.println(read);
+                }
+            } catch (EOFException e) {
+                serverSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
