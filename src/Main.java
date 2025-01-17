19d4d959e7783ca9ca020a7adc463f260b4d047e
potados99 <potados99@gmail.com>
2025-01-17 12:31:32 +0900

    refactor: 기본 print 예제를 별도의 파일로 분리해요

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,7 @@
-import java.net.*;
-import java.io.*;
+import basic.BasicPrintExample;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-
-        try {
-            ServerSocket serverSocket = new ServerSocket();
-
-            serverSocket.bind(new InetSocketAddress("0.0.0.0", 8080));
-
-            Socket socket = serverSocket.accept();
-
-            DataInputStream inputStream = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
-
-            try {
-                while (true) {
-                    byte read = inputStream.readByte();
-                    System.out.println(read);
-                }
-            } catch (EOFException e) {
-                serverSocket.close();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        new BasicPrintExample();
     }
 }
---
+++ src/basic/BasicPrintExample.java
@@ -0,0 +1,36 @@
+package basic;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class BasicPrintExample {
+    void run() {
+        System.out.println("Hello, World!");
+
+        try {
+            ServerSocket serverSocket = new ServerSocket();
+
+            serverSocket.bind(new InetSocketAddress("0.0.0.0", 8080));
+
+            Socket socket = serverSocket.accept();
+
+            DataInputStream inputStream = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+
+            try {
+                while (true) {
+                    byte read = inputStream.readByte();
+                    System.out.println(read);
+                }
+            } catch (EOFException e) {
+                serverSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
