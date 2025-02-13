bf669eb43e63f66c08f6d005ec373747b6d291a3
algosketch <algosketch@gmail.com>
2025-02-13 17:15:00 +0900

    fix: 아니 HttpRequest 속에 Map 객체 레퍼런스만 달고 초기화도 안하는 미123친 자바 바보짓 한거 수습
    Co-authored-by: potados99 <potados99@gmail.com>

--- src/winter/http/HttpRequest.java
+++ src/winter/http/HttpRequest.java
@@ -1,10 +1,11 @@
 package winter.http;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class HttpRequest {
     String method;
     String uri;
-    Map<String, String> headers;
+    Map<String, String> headers = new HashMap<String, String>();
     String body;
 }
